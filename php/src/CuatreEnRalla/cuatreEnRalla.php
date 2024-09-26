65a54fcc32d4b08b15ef82f706024758c6dec81c
Kokon4 <kicodbz1@gmail.com>
2024-09-26 16:10:06 +0200

    metodo inicialitzar graella

--- php/src/CuatreEnRalla/cuatreEnRalla.php
+++ php/src/CuatreEnRalla/cuatreEnRalla.php
@@ -1,7 +1,18 @@
 <?php
 include_once "functions.php";
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuatre en Ralla</title>
+    <link rel="stylesheet" href="CuatreEnRalla.css"> 
+</head>
+<body>
+    <h1>Benvingut al Cuatre en Ralla</h1>
+    <?php inicialitzarGraella(); ?>
+</body>
+</html>
 
--- php/src/CuatreEnRalla/functions.php
+++ php/src/CuatreEnRalla/functions.php
@@ -1,6 +1,17 @@
 <?php
+    function inicialitzarGraella() {
 
-function inicialitziarGraella(){
+        echo '<table style="border-collapse: collapse;">'; 
+        for ($i = 0; $i < 6; $i++) { // 6 filas
+            echo '<tr>'; 
+            for ($j = 0; $j < 7; $j++) { // 7 columnas
+                echo '<td>[ ]</td>';
+            }
+            echo '</tr>'; 
+        }
+        echo '</table>'; 
+    }
+
+
     
-}
 ?>
