8a37e998f19ec3b551da0a19a9047978221ebe3b
Kokon4 <kicodbz1@gmail.com>
2024-09-26 15:54:28 +0200

    Rama

--- php/src/CuatreEnRalla/functions.php
+++ php/src/CuatreEnRalla/functions.php
@@ -1,12 +1,6 @@
 <?php
 
 function inicialitziarGraella(){
-    $graella = array(array());
-    for($files = 0; $files < 6; $files++) {
-        $graella[$files] = "";
-        for($columnes = 0; $columnes < 7; $columnes++){
-            $graella[$files][$columnes] = "";
-        }
-    }
+    
 }
 ?>
