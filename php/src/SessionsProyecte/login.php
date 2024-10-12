d1b17dcc033c932e18c19f9e46cf6061a80ed1de
Kokon4 <kicodbz1@gmail.com>
2024-10-12 11:48:50 +0200

    Espais llevats

--- php/src/SessionsProyecte/login.php
+++ php/src/SessionsProyecte/login.php
@@ -1,5 +1,4 @@
 <?php
-
         session_start();
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -10,11 +9,9 @@
             $_SESSION['nom'] = $nom;
             $_SESSION['contrasenya'] = $contrasenya;
 
-
             if (isset($_POST['recordarme'])) {
                 setcookie('nom', $nom, time() + (7 * 24 * 60 * 60)); 
             }
-
             header("Location: pantallaInicial.php");
             exit();
         }
@@ -27,21 +24,15 @@
     <title>Inici de Sessió</title>
 </head>
 <body>
-
     <h2>Benvingut a l'aplicació</h2>
-
     <form action="login.php" method="POST">
         <label for="nom">Nom Usuari: </label>
         <input type="text" id="nom" name="nom" required><br><br>
-
         <label for="contrasenya">Contrasenya: </label>
         <input type="password" id="password" name="contrasenya" required><br><br>
-
         <label for="recordarme">Recordar-me: </label>
         <input type="checkbox" id="recordarme" name="recordarme"><br><br>
-
         <input type="submit" value="Enviar">
     </form>
-
 </body>
 </html>
